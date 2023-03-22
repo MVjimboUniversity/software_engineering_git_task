e536998589f95339a3c81b4a1722ae84fb18b324
MVjimbo <maslo-86@ya.ru>
2023-03-22 17:45:23 +0300

    Adding mult in main.py

--- main.py
+++ main.py
@@ -2,5 +2,10 @@ def counter(n: int) -> int:
     return sum(range(n+1))
 
 
+def mult(n: int) -> int:
+    res = [i for i in range(1,n+1)]
+    return sum(res)
+
+
 if __name__ == "__main__":
-    print(counter(5))
+    print(mult(5))
